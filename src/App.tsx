d9966f24af4da0ef1b32f11868671eccb6a6a67c
Orazbek Daurenov <oraz.daurenov@gmail.com>
2023-12-16 00:40:45 +0100

    made shape of Country Prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Country {
+  name: { common: string };
+  population: string;
+  region: string;
+  capital: string;
+  currencies: string;
+}
+
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get<Country[]>("https://restcountries.com/v3.1/all")
+      .then((res) => console.log(res.data[5].currencies));
+  });
   return <></>;
 }
 
