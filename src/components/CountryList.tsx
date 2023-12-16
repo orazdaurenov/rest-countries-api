317dc26a31c8625105346a85fc9f2873124301a1
Orazbek Daurenov <oraz.daurenov@gmail.com>
2023-12-16 16:48:02 +0100

    minor change on box div

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -22,16 +22,21 @@ const CountryList = () => {
 
   return (
     <>
-      {countries.map((country) => (
-        <div key={country.id} className="">
-          {country.id}
-          <img src={country.flags.svg} alt={country.name.common} />
-          <h1 className="">{country.name.common}</h1>
-          <p className="">{country.population}</p>
-          <p className="">{country.region}</p>
-          <p className="">{country.capital}</p>
-        </div>
-      ))}
+      <div className="bg-[#fafafa] w-[375px] h-[3305px]">
+        {countries.map((country) => (
+          <div
+            key={country.id}
+            className="bg-[#d6d2d2] rounded-[5px] w-[264px] h-[336px] mb-5"
+          >
+            {country.id}
+            <img src={country.flags.svg} alt={country.name.common} />
+            <h1 className="">{country.name.common}</h1>
+            <p className="">Population: {country.population}</p>
+            <p className="">Region: {country.region}</p>
+            <p className="">Capital: {country.capital}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
