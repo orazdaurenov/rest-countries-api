e26a7aa55a7798aec225652532fadc3012433fc6
Orazbek Daurenov <oraz.daurenov@gmail.com>
2023-12-16 01:17:01 +0100

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Country {
+  id: number;
   name: { common: string };
   population: string;
   region: string;
@@ -10,14 +11,27 @@ interface Country {
 }
 
 function App() {
-  const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState<Country[]>([]);
 
   useEffect(() => {
     axios
       .get<Country[]>("https://restcountries.com/v3.1/all")
-      .then((res) => console.log(res.data[5].currencies));
-  });
-  return <></>;
+      .then((res) => setCountries(res.data));
+  }, []);
+
+  return (
+    <>
+      {countries.map((country) => (
+        <div key={country.id} className="container flex mb-5 gap-3">
+          {country.id}
+          <h1 className="">{country.name.common}</h1>
+          <p className="">{country.population}</p>
+          <p className="">{country.region}</p>
+          <p className="">{country.capital}</p>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
